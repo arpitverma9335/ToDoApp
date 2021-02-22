a2a798442f2c85fa9765cbaa79eb17036e4b837a
arpitverma9335 <asme.arpitverma19@gmail.com>
2021-02-22 11:49:21 +0530

    time left add

--- todoapp/views.py
+++ todoapp/views.py
@@ -2,8 +2,10 @@
 from django.shortcuts import get_object_or_404,render,HttpResponseRedirect,redirect
 from django.contrib import messages
 from .models import List , user_ip
+import datetime as dtm
 from datetime import datetime , timezone
 from django.db.models import Count
+import pytz
 
 # Create your views here.
 def index_func(request):
@@ -25,7 +27,24 @@ def index_func(request):
       current_id = request.user.id
       activities_1= List.objects.filter(user__id=current_id , completed='True').order_by('date').values()
       activities_2 = List.objects.filter(user__id=current_id , completed='False').order_by('date').values()
-      return render(request,'index.html',{'date':date_list[-1::-1] , 'count':count_list[-1::-1] , 'activities_crit':activities_1 , 'activities_ncrit':activities_2})
+      time_rem_1 , time_rem_2 = [] , []
+      for i in activities_1:
+            dt_now = datetime.now(tz=pytz.timezone('ASia/Kolkata'))
+            dt_due = i.get('date')
+            t_left = dt_due - dt_now
+            if (t_left).total_seconds() < 0:
+                  time_rem_1.append('Time is Up')
+                  continue
+            time_rem_1.append(t_left)
+      for i in activities_2:
+            dt_now = datetime.now(tz=pytz.timezone('ASia/Kolkata'))
+            dt_due = i.get('date')
+            t_left = dt_due - dt_now
+            if (t_left).total_seconds() < 0:
+                  time_rem_2.append('Time is Up')
+                  continue
+            time_rem_2.append(t_left)
+      return render(request,'index.html',{'date':date_list[-1::-1] , 'count':count_list[-1::-1] , 'activities_crit':zip(activities_1 , time_rem_1) , 'activities_ncrit':zip(activities_2 , time_rem_2)})
 
 def contact_func(request):
       email = 'asme.arpitverma19@gmail.com'
